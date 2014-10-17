7b6275994ff320903027840b1f8ef3f9f2e4c9d8
Gilles Dartiguelongue <gdartiguelongue@ocs.online.net>
2014-10-17 15:59:02 +0200

    Ubuntu scripts moved to official github repository

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# GENTOO
-tar -czf gentoo.tar.gz gentoo
-
-# UBUNTU
-VERSION=$(cat ubuntu/DEBIAN/control | grep -i version | cut -d\  -f2)
-dpkg-deb --build ubuntu ocs-image-config-$VERSION.deb
-tar -czf ubuntu.tar.gz ubuntu
